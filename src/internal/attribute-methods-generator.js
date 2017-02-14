72bf36dcb3a61082f6996e052150f4d0cbfcc3c9
Pieter Beulque <pieterbeulque@gmail.com>
2017-02-14 16:32:40 +0100

    Add number (float & int) attribute types to `define` method

---
+++ src/internal/attribute-methods-generator.js
@@ -0,0 +1,93 @@
+const noop = function () {};
+
+const generateStringAttributeMethods = function (attribute) {
+	const getter = function () {
+		return this.el.getAttribute(attribute) || undefined;
+	};
+
+	const setter = function (to) {
+		if (!!to) {
+			this.el.setAttribute(attribute, to);
+		} else {
+			this.el.removeAttribute(attribute);
+		}
+	};
+
+	return { getter, setter };
+};
+
+const generateBoolAttributeMethods = function (attribute) {
+	const getter = function () {
+		return !!this.el.hasAttribute(attribute);
+	};
+
+	const setter = function (to) {
+		if (!!to) {
+			this.el.setAttribute(attribute, attribute);
+		} else {
+			this.el.removeAttribute(attribute);
+		}
+	};
+
+	return { getter, setter };
+};
+
+const generateIntegerAttributeMethods = function (attribute) {
+	const getter = function () {
+		return parseInt(this.el.getAttribute(attribute), 10);
+	};
+
+	const setter = function (to) {
+		const parsed = parseInt(to, 10);
+
+		if (!Number.isNaN(parsed)) {
+			this.el.setAttribute(attribute, parsed);
+		} else {
+			console.warn(`Could not set ${attribute} to ${to}`);
+			this.el.removeAttribute(attribute);
+		}
+	};
+
+	return { getter, setter };
+};
+
+const generateNumberAttributeMethods = function (attribute) {
+	const getter = function () {
+		return parseFloat(this.el.getAttribute(attribute));
+	};
+
+	const setter = function (to) {
+		const parsed = parseFloat(to);
+
+		if (!Number.isNaN(parsed)) {
+			this.el.setAttribute(attribute, parsed);
+		} else {
+			console.warn(`Could not set ${attribute} to ${to}`);
+			this.el.removeAttribute(attribute);
+		}
+	};
+
+	return { getter, setter };
+};
+
+const generateAttributeMethods = function (attribute, type = 'string') {
+	if (type === 'bool') {
+		return generateBoolAttributeMethods(name);
+	} else if (type === 'int' || type === 'integer') {
+		return generateIntegerAttributeMethods(name);
+	} else if (type === 'float' || type === 'number') {
+		return generateNumberAttributeMethods(name);
+	} else if (type === 'string') {
+		return generateStringAttributeMethods(name);
+	} else {
+		return { getter: noop, setter: noop };
+	}
+};
+
+export {
+	generateBoolAttributeMethods,
+	generateIntegerAttributeMethods,
+	generateStringAttributeMethods,
+	generateNumberAttributeMethods,
+	generateAttributeMethods
+};
--- src/util/define.js
+++ src/util/define.js
@@ -1,36 +1,5 @@
 import { addProperty } from '../internal/decorators';
-
-const generateStringAttributeMethods = function (attribute) {
-	const getter = function () {
-		return this.el.getAttribute(attribute) || undefined;
-	};
-
-	const setter = function (to) {
-		if (!!to) {
-			this.el.setAttribute(attribute, to);
-		} else {
-			this.el.removeAttribute(attribute);
-		}
-	};
-
-	return { getter, setter };
-};
-
-const generateBoolAttributeMethods = function (attribute) {
-	const getter = function () {
-		return !!this.el.hasAttribute(attribute);
-	};
-
-	const setter = function (to) {
-		if (!!to) {
-			this.el.setAttribute(attribute, attribute);
-		} else {
-			this.el.removeAttribute(attribute);
-		}
-	};
-
-	return { getter, setter };
-};
+import { generateAttributeMethods } from '../internal/attribute-methods-generator';
 
 export default function defineCustomElement(tag, options = {}) {
 	// Attach all passed attributes to the passed controller
@@ -46,13 +15,7 @@ export default function defineCustomElement(tag, options = {}) {
 				const type = attribute.type || 'string';
 				const name = attribute.attribute;
 
-				const { getter, setter } = (function () {
-					if (type === 'string') {
-						return generateStringAttributeMethods(name);
-					} else if (type === 'bool') {
-						return generateBoolAttributeMethods(name);
-					}
-				}());
+				const { getter, setter } = generateAttributeMethods(name, type);
 
 				addProperty(options.controller, name, getter, setter);
 			}
