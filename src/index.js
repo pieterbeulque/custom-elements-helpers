afbdbe6484065c707f863d65a835c55180f0f587
Pieter Beulque <pieterbeulque@gmail.com>
2017-02-17 17:15:53 +0100

    Forgot build

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ export { default as BaseController } from './controllers/base';
 export { default as media } from './attributes/media';
 
 // Elements
+export { default as keyTrigger } from './elements/key-trigger';
 export { default as smoothState } from './elements/smooth-state';
 
 // Utilities
