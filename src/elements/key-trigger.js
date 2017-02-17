1ccd704f9d550bdb82110a3b87459f24c59a7ea8
Pieter Beulque <pieterbeulque@gmail.com>
2017-02-17 17:09:00 +0100

    Add key-trigger element

---
+++ src/elements/key-trigger.js
@@ -0,0 +1,34 @@
+import BaseController from '../controllers/base';
+
+export default {
+	attributes: [
+		{ attribute: 'key', type: 'int' }
+	],
+	controller: class extends BaseController {
+
+		init() {
+			this.elements = this.elements || {};
+
+			if (this.el.hasAttribute('href')) {
+				this.elements.target = this;
+			} else {
+				this.elements.target = this.el.querySelector('[href]')
+			}
+
+			return this;
+		}
+
+		bind() {
+			if (this.elements.target) {
+				this.on('keyup', e => {
+					if (e.which === this.key) {
+						this.elements.target.click();
+					}
+				}, document.body);
+			}
+
+			return this;
+		}
+
+	}
+});
