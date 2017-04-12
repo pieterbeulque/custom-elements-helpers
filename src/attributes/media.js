ba2db02855429acb0fbcbe5a0679765cb2166ebb
Pieter Beulque <pieterbeulque@gmail.com>
2017-04-12 15:15:25 +0200

    Extend media attribute functionality

--- src/attributes/media.js
+++ src/attributes/media.js
@@ -3,6 +3,10 @@ import { addMethod, addGetter } from '../internal/decorators';
 export default class AttrMedia {
 
 	static attachTo(customElement) {
+		const noop = function () {};
+
+		let mq;
+
 		// Adds customElement.media
 		// @return string 		Value of `media=""` attribute
 		addGetter(customElement, 'media', function getMediaAttribute() {
@@ -23,8 +27,6 @@ export default class AttrMedia {
 		// @return Promise
 		addMethod(customElement, 'whenMediaMatches', function whenMediaMatches() {
 			const defer = new Promise((resolve) => {
-				let mq;
-
 				const handler = function () {
 					if (mq.matches) {
 						resolve();
@@ -33,7 +35,7 @@ export default class AttrMedia {
 				};
 
 				if ('matchMedia' in window) {
-					mq = window.matchMedia(this.media);
+					mq = mq || window.matchMedia(this.media);
 					mq.addListener(handler);
 					handler(mq);
 				} else {
@@ -42,6 +44,45 @@ export default class AttrMedia {
 			});
 
 			return defer;
+		});
+
+		// Adds customElements.whenMediaUnmatches()
+		// @return Promise
+		addMethod(customElement, 'whenMediaUnmatches', function whenMediaUnmatches() {
+			const defer = new Promise((resolve) => {
+				const handler = function () {
+					if (mq.matches) {
+						resolve();
+						mq.removeListener(handler);
+					}
+				};
+
+				if ('matchMedia' in window) {
+					mq = mq || window.matchMedia(this.media);
+					mq.addListener(handler);
+					handler(mq);
+				} else {
+					resolve();
+				}
+			});
+
+			return defer;
+		});
+
+		addMethod(customElement, 'watchMedia', function watchMedia(match = noop, unmatch = noop) {
+			const handler = function () {
+				if (mq.matches) {
+					match();
+				} else {
+					unmatch();
+				}
+			};
+
+			if ('matchMedia' in window) {
+				mq = mq || window.matchMedia(this.media);
+				mq.addListener(handler);
+				handler(mq);
+			}
 		});
 	}
 
