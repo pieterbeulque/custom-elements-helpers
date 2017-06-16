b97ed6646803aa3dc5cebd722e1b4884ddfa033a
Pieter Beulque <pieterbeulque@gmail.com>
2017-06-16 08:23:51 +0200

    Convert dash-separated attribute names to camelcased property names

--- src/internal/decorators.js
+++ src/internal/decorators.js
@@ -1,3 +1,13 @@
+const convertAttributeToPropertyName = function (attribute) {
+	return attribute.split('-').reduce((camelcased, part, index) => {
+		if (index === 0) {
+			return part;
+		}
+
+		return camelcased + part[0].toUpperCase() + part.substr(1);
+	});
+};
+
 const addMethod = function (customElement, name, method) {
 	if (typeof customElement.prototype[name] !== 'undefined') {
 		console.warn(`${customElement.name} already has a property ${name}`);
@@ -7,35 +17,41 @@ const addMethod = function (customElement, name, method) {
 };
 
 const addGetter = function (customElement, name, method) {
+	const getterName = convertAttributeToPropertyName(name);
+
 	if (typeof customElement.prototype[name] !== 'undefined') {
-		console.warn(`${customElement.name} already has a property ${name}`);
+		console.warn(`${customElement.name} already has a property ${getterName}`);
 	}
 
-	Object.defineProperty(customElement.prototype, name, {
+	Object.defineProperty(customElement.prototype, getterName, {
 		configurable: false,
 		get: method,
 	});
 };
 
 const addSetter = function (customElement, name, method) {
+	const setterName = convertAttributeToPropertyName(name);
+
 	if (typeof customElement.prototype[name] !== 'undefined') {
-		console.warn(`${customElement.name} already has a property ${name}`);
+		console.warn(`${customElement.name} already has a property ${setterName}`);
 	}
 
-	Object.defineProperty(customElement.prototype, name, {
+	Object.defineProperty(customElement.prototype, setterName, {
 		configurable: false,
 		set: method,
 	});
 };
 
 const addProperty = function (customElement, name, getter = null, setter = null) {
+	const propertyName = convertAttributeToPropertyName(name);
+
 	if (typeof customElement.prototype[name] !== 'undefined') {
-		console.warn(`${customElement.name} already has a property ${name}`);
+		console.warn(`${customElement.name} already has a property ${propertyName}`);
 	}
 
 	const noop = function () {};
 
-	Object.defineProperty(customElement.prototype, name, {
+	Object.defineProperty(customElement.prototype, propertyName, {
 		configurable: false,
 		get: typeof getter === 'function' ? getter : noop,
 		set: typeof setter === 'function' ? setter : noop,
